40d9cc5f965488d473dff7cad85454a65133670e
Samuel Pelsy <samuel.pelsy@gmail.com>
2025-04-16 13:53:39 +0200

    fix(build warnings)

--- src/routes/flash/+page.svelte
+++ src/routes/flash/+page.svelte
@@ -144,7 +144,7 @@
   {#if visible}
     <div class="bg-white rounded-xl shadow-2xl max-w-md w-full p-6 space-y-8" in:fade={{ duration: 300, delay: 50 }} >
       <div class="flex items-center justify-left">
-        <a href="/" class="text-gray-600 hover:text-gray-800">
+        <a href="/" class="text-gray-600 hover:text-gray-800"  title="Retour à la page d'accueil" aria-label="Retour à la page d'accueil">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
